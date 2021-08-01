bb0b28a94aa82ce7985e9b40f128c0f44e03e46c
Paul Adams <ghusiyy@gmail.com>
2021-08-01 01:25:23 -0700

    set up test proxy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,9 @@
 <script context="module" lang="ts">
 	export const prerender = true;
+	fetch('/api')
+		.then((d) => d.json())
+		.then((res) => console.log(res))
+		.catch((error) => console.error('error'));
 </script>
 
 <script lang="ts">
@@ -11,22 +15,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Hadith Toolkit UI</h1>
 </section>
 
 <style>
@@ -40,20 +29,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,18 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: () => ({
+			server: {
+				proxy: {
+					'/api': {
+						target: 'http://jsonplaceholder.typicode.com/photos',
+						changeOrigin: true,
+						rewrite: (path) => path.replace(/^\/api/, '')
+					}
+				}
+			}
+		})
 	}
 };
 
