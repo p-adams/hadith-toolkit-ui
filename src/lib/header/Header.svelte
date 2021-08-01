4a787f2c293dc813abc6bb2fea4c300ef58f5d34
Paul Adams <ghusiyy@gmail.com>
2021-08-01 13:45:28 -0700

    create basic table to display biographies

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -4,27 +4,16 @@
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
+	<div class="corner" />
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
 			<li class:active={$page.path === '/biographies'}>
 				<a sveltekit:prefetch href="/biographies">Biographies</a>
 			</li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
 
 	<div class="corner">
@@ -43,34 +32,10 @@
 		height: 3em;
 	}
 
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
 	nav {
 		display: flex;
 		justify-content: center;
 		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
 	}
 
 	ul {
@@ -82,7 +47,6 @@
 		justify-content: center;
 		align-items: center;
 		list-style: none;
-		background: var(--background);
 		background-size: contain;
 	}
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,11 +5,14 @@
 <script lang="ts">
 	import { db } from '../firebase';
 	import { onMount } from 'svelte';
+	let biographies = [];
 	onMount(() => {
 		db.ref()
 			.limitToFirst(10)
 			.on('value', (snapshot) => {
-				console.log(snapshot.val());
+				// console.log(snapshot.val());
+				biographies = [...snapshot.val()];
+				console.log('bio', biographies);
 			});
 	});
 </script>
@@ -20,9 +23,17 @@
 
 <section>
 	<h1>Hadith Toolkit UI</h1>
+	<div class="table-container">
+		<div class="header">id</div>
+		<div class="header">data</div>
+		{#each biographies as biography}
+			<div class="row cell">{biography.id}</div>
+			<div class="row cell data">{biography.data}</div>
+		{/each}
+	</div>
 </section>
 
-<style>
+<style lang="scss">
 	section {
 		display: flex;
 		flex-direction: column;
@@ -30,7 +41,30 @@
 		align-items: center;
 		flex: 1;
 	}
-
+	.table-container {
+		border: 1px solid lightgray;
+		border-radius: 2px;
+		display: grid;
+		grid-template-columns: 70px auto;
+		.header,
+		.row {
+			border: 1px solid lightgrey;
+			text-align: center;
+			padding: 10px;
+		}
+		.header {
+			text-transform: uppercase;
+			font-weight: 600;
+		}
+		.row {
+			&.data {
+				display: flex;
+				justify-content: flex-end;
+				text-align: right;
+				align-items: center;
+			}
+		}
+	}
 	h1 {
 		width: 100%;
 	}
